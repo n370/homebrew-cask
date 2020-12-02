0e298c34308df2b36431d1ee4a322ffc2a239d57
Dylson Valente Neto <ama@n370.info>
2020-12-02 19:00:14 -0300

    Add lightsailctl AWS CLI plugin

---
+++ Casks/lightsailctl.rb
@@ -0,0 +1,9 @@
+cask "lightsailctl" do
+  version "1.0.0"
+  url "https://s3.us-west-2.amazonaws.com/lightsailctl/latest/darwin-amd64/lightsailctl"
+  name "AWS Lighsail Container Services Plug-In"
+  desc "AWS CLI plugin for publishing images to AWS Lightsail container services"
+  homepage "https://lightsail.aws.amazon.com/ls/docs/en_us/articles/amazon-lightsail-install-software"
+
+  # WIP
+end
